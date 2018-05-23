35ee693cdc3ef8409e8a2037f00eca8b9ea19146
BCshirdavani <beau.shirdavani@bellevuecollege.edu>
2018-05-22 20:47:52 -0700

    interface in progress
    still working on key:value extractor

--- src/LRU_cache/CacheClient.java
+++ src/LRU_cache/CacheClient.java
@@ -2,4 +2,19 @@ package LRU_cache;
 
 public class CacheClient {
 
+	public void main(String[] args){
+		LRUcache cache = new LRUcache(10);
+		TextFileStrategy dataFile = new TextFileStrategy("./src/LRU_cache/FileTest_01.txt");
+
+		int myKey = 4;
+		System.out.println("searching for key: " + myKey);
+		Object myValue = dataFile.getData(myKey);
+		System.out.println(myValue);
+		cache.set(myKey, myValue);
+
+		System.out.println("printing from cache...");
+		System.out.println(cache.get(myKey));
+
+	}
+
 }
--- src/LRU_cache/TextFileStrategy.java
+++ src/LRU_cache/TextFileStrategy.java
@@ -1,14 +1,53 @@
 package LRU_cache;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class TextFileStrategy implements GetDataStrategy {
+	String filePath;
+	File file;
+	Scanner sc;
+
+	//	CONSTRUCTOR
+	public TextFileStrategy(String path){
+		this.filePath = path;
+		this.file = new File(path);
+		try {
+			this.sc = new Scanner(file);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+
+
+	// 	Find value at KEY
+	//	for a comma separated .txt file
 	@Override
 	public Object getData(int key) {
+		int currentKey = -99;
+		String line;
+		String[] lineElements;
+		Object value = null;
 		//TODO: implement file.txt key value search and extract
-
-
-
-
+		while( (sc.hasNextLine()) && (currentKey != key) ){
+			line = sc.nextLine();
+			lineElements = line.split(",");
+			currentKey = Integer.parseInt(lineElements[0]);
+			value = lineElements[1];
+		}
 		//	if not found
-		return -1;
+		if (currentKey != key){
+			return -1;
+		}
+		//	if found
+		else{
+			return value;
+		}
 	}
+
+
+
+
+
 }
